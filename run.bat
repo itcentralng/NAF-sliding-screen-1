fc6c91462b99619fefff2bbb29f606e6a339ff9e
Nasir Mustapha <teboy.com@gmail.com>
2025-09-04 10:06:16 +0100

    Update run.bat to launch Edge in fullscreen kiosk mode

--- run.bat
+++ run.bat
@@ -4,5 +4,5 @@ start /MIN "" python -m http.server 8000
 start /MIN "" python main.py
 timeout /t 5 /nobreak
 
-:: Uncomment the line below if you prefer to use Microsoft Edge
-start "" "msedge" --app=http://127.0.0.1:8000/welcome.html
+:: Launch Edge in fullscreen kiosk mode
+start "" "msedge" --kiosk http://127.0.0.1:8000/welcome.html
